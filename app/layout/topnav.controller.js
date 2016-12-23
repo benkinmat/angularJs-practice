fef468fe40265b7a0a282090981140fcf74ca028
Tuan Anh Ho Dien <benkinmat@gmail.com>
2016-12-23 15:55:30 +0000

    Update the topnav controller

--- app/layout/topnav.controller.js
+++ app/layout/topnav.controller.js
@@ -8,7 +8,6 @@ angular.module('app.layout')
 TopNav.$inject = ['$scope'];
 
 function TopNav(scope){
-<<<<<<< HEAD
     scope.states = {
         introduction: [
             {name: 'Home',      sref: 'app.home'},
@@ -19,10 +18,4 @@ function TopNav(scope){
             {name: "Note",      sref: 'app.note'}
         ]
     };
-=======
-    scope.states = [
-        { name: 'Home',     sref: 'app.home'},
-        { name: 'About',    sref: 'app.about'}
-    ];
->>>>>>> db31b9144b4fecf4fb599a0ffc5eccd6a70902d6
 }
